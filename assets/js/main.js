0cdd852f620b59752761ada41499d93290c42dee
Leyva Roldan <giealejandra17@gmail.com>
2022-03-23 20:57:49 -0500

    services part 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,6 +71,27 @@ tabs.forEach(tab =>{
 
 
 /* services modal */ 
+const modalViews = document.querySelectorAll('.services__modal'),
+	modalBtns = document.querySelectorAll('.services__button'),
+	modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+	modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () =>{
+		modal(i)
+	})
+})
+
+modalCloses.forEach((modalClose) => {
+	modalClose.addEventListener('click', () =>{
+		modalViews.forEach((modalView) =>{
+			modalView.classList.remove('active-modal')
+		})
+	})
+})
 
 /* portfolio swiper */ 
 
@@ -79,3 +100,7 @@ tabs.forEach(tab =>{
 /* scroll sections active link */ 
 
 /* change background header */ 
+
+/* show scroll top*/ 
+
+/* dark light time */ 
