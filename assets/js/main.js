c5461673473e298a54328f30ce984c8dde49e661
Leyva Roldan <giealejandra17@gmail.com>
2022-03-25 15:23:18 -0500

    portfolio 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,17 +94,18 @@ modalCloses.forEach((modalClose) => {
 })
 
 /* portfolio swiper */ 
-let swiper = new swiper('.portfolio__container', {
+let swiper = new Swiper('.portfolio__container', {
 	cssMode: true,
+	loop: true, 
+
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
 	pagination: {
-		el: '.swiper-pagination'
+		el: '.swiper-pagination',
+		clickable: true,
 	},
-	mousewheel: true,
-	keyboard: true,
 });
 
 
