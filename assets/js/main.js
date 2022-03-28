9c17bd0b918b1a3c6bf44a7d1cdc4b30962fedda
Leyva Roldan <giealejandra17@gmail.com>
2022-03-27 20:59:02 -0500

    active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,7 +148,14 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /* change background header */ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /* show scroll top*/ 
+
 
 /* dark light time */ 
